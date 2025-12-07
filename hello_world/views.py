3dfd3f2ba3d4ba8bf8d1a57822991c0ef47b836f
Dawid 99891 <dawid.radzioooi@gmail.com>
2025-12-07 09:09:43 -0500

    Poprawa funkcji get_formatted i views.py, aby testy przeszły

--- hello_world/views.py
+++ hello_world/views.py
@@ -1,28 +1,17 @@
-from flask import Flask, request, jsonify
-
-
-app = Flask(__name__)
-
+from hello_world import app
+from hello_world.formater import get_formatted
+from hello_world.formater import SUPPORTED
+from flask import request
 
 moje_imie = "Dawid"
 msg = "Hello World!"
-
-
-SUPPORTED = ["json", "text"]
-
-
-def get_formatted(message, name, output):
-    if output == "json":
-        return jsonify({"imie": name, "msg": message})
-    return f"{message} {name}"
-
 
 @app.route('/')
 def index():
     output = request.args.get('output', '').lower()
     return get_formatted(msg, moje_imie, output)
 
-
 @app.route('/outputs')
 def supported_output():
     return ", ".join(SUPPORTED)
+

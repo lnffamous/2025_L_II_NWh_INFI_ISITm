e3037194fc5d1ace7daf2bb00e5c9d46f6e56d86
Dawid 99891 <dawid.radzioooi@gmail.com>
2025-12-07 08:46:05 -0500

    Naprawa formatowania i dodanie brakujących funkcji

--- hello_world/views.py
+++ hello_world/views.py
@@ -5,15 +5,24 @@ app = Flask(__name__)
 moje_imie = "Dawid"
 msg = "Hello World!"
 
-SUPPORTED = ["json", "xml", "yaml"]
 
-def get_formatted(msg, name, output):
-    return f"{msg} {name} ({output})"
+def get_formatted(msg, name, output_format):
+    if output_format == "upper":
+        return f"{msg.upper()} {name.upper()}"
+    elif output_format == "lower":
+        return f"{msg.lower()} {name.lower()}"
+    else:
+        return f"{msg} {name}"
+
+
+SUPPORTED = ["upper", "lower"]
+
 
 @app.route('/')
 def index():
-    output = request.args.get('output')
+    output = request.args.get('output', 'upper')
     return get_formatted(msg, moje_imie, output.lower())
+
 
 @app.route('/outputs')
 def supported_output():

17ff81deb4071ff20d931d8d1fc1c86300737d9d
Dawid 99891 <dawid.radzioooi@gmail.com>
2025-12-07 08:51:11 -0500

    Poprawa formatowania PEP8 i implementacja brakujących funkcji

--- hello_world/views.py
+++ hello_world/views.py
@@ -1,23 +1,28 @@
 from flask import Flask, request, jsonify
 
+
 app = Flask(__name__)
+
 
 moje_imie = "Dawid"
 msg = "Hello World!"
 
+
 SUPPORTED = ["json", "text"]
+
 
 def get_formatted(message, name, output):
     if output == "json":
         return jsonify({"imie": name, "msg": message})
     return f"{message} {name}"
 
+
 @app.route('/')
 def index():
     output = request.args.get('output', '').lower()
     return get_formatted(msg, moje_imie, output)
 
+
 @app.route('/outputs')
 def supported_output():
     return ", ".join(SUPPORTED)
-

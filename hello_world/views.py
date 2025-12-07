aaafb9179e98f881db755fadb2404743d77a745e
Dawid 99891 <dawid.radzioooi@gmail.com>
2025-12-07 08:49:33 -0500

    Poprawa endpointu /, zwracanie JSON dla output=json

--- hello_world/views.py
+++ hello_world/views.py
@@ -1,29 +1,23 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
 moje_imie = "Dawid"
 msg = "Hello World!"
 
+SUPPORTED = ["json", "text"]
 
-def get_formatted(msg, name, output_format):
-    if output_format == "upper":
-        return f"{msg.upper()} {name.upper()}"
-    elif output_format == "lower":
-        return f"{msg.lower()} {name.lower()}"
-    else:
-        return f"{msg} {name}"
-
-
-SUPPORTED = ["upper", "lower"]
-
+def get_formatted(message, name, output):
+    if output == "json":
+        return jsonify({"imie": name, "msg": message})
+    return f"{message} {name}"
 
 @app.route('/')
 def index():
-    output = request.args.get('output', 'upper')
-    return get_formatted(msg, moje_imie, output.lower())
-
+    output = request.args.get('output', '').lower()
+    return get_formatted(msg, moje_imie, output)
 
 @app.route('/outputs')
 def supported_output():
     return ", ".join(SUPPORTED)
+

c199663cd4e08444b9d47cfd9358618f9d896f32
lnffamous <dawid.radzioooi@gmail.com>
2025-12-16 17:26:11 +0100

    Poprawki

--- hello_world/views.py
+++ hello_world/views.py
@@ -6,12 +6,13 @@ from flask import request
 moje_imie = "Dawid"
 msg = "Hello World!"
 
+
 @app.route('/')
 def index():
     output = request.args.get('output', '').lower()
     return get_formatted(msg, moje_imie, output)
 
+
 @app.route('/outputs')
 def supported_output():
     return ", ".join(SUPPORTED)
-
